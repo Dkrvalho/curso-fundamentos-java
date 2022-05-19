69c21348d41da6c370ab96f6cfe917af403664a4
Dkrvalho <eliasdecarvalho.foto@gmail.com>
2022-05-19 07:24:43 -0300

    Exercício 2

---
+++ src/dio/findamentos/java/aula2/Main.java
@@ -0,0 +1,52 @@
+package dio.findamentos.java.aula2;
+
+//Classe de Exemplo para o exercício da Aula 2 de Variáveis, Tipos de Dods e Operadores Aritméticos.
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -100000000000;
+        int i2 = 28500;
+
+        long l1 = 12345678901211L;
+        long l2 = 20000000000000L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 3.14159265;
+        double d2 = 3.14159265;
+
+        char c1 = 'T';
+        char c2 = 'Y';
+
+        String t1 = "Letra T";
+        String t2 = "Letra Y";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
